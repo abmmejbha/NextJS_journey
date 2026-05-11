175d58294765320ddfe496c53cf1fd3592784478
A.B.M. Mejbha Saimum Chapol <146228288+abmmejbha@users.noreply.github.com>
2026-05-11 21:14:12 +0600

    added skeleton loading page

--- my-app/src/app/users/loading.js
+++ my-app/src/app/users/loading.js
@@ -1,9 +1,20 @@
 export default function loading() {
-    return (
-        <div className="felx items-center justify-center min-h-screen">
-            <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-500 ">
-                <h2 className="ml-4 text-semibold">Loading users...</h2>
+  const skeletonCards = Array(10).fill(0);
+
+  return (
+    <div className="p-8">
+      <div className="animate-pulse rounded mb-6 h-8 w-48 bg-gray-300 "></div>
+        
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          {skeletonCards.map((_, index) => (
+            <div key={index} className="p-4 border rounded shadow-sm space-y-3">
+              <div className="h-5 w-3/4 bg-gray-200 rounded animate-pulse"></div>
+              <div className="h-4 w-1/2 bg-gray-200 rounded animate-pulse"></div>
+              <div className="h-10 w-32 bg-gray-200 rounded animate-pulse"></div>
             </div>
+          ))}
         </div>
-    )
+
+    </div>
+  );
 }
