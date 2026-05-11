c8797ec4d17cd1a45ce0e51dac7be5920292fb5d
A.B.M. Mejbha Saimum Chapol <146228288+abmmejbha@users.noreply.github.com>
2026-05-11 20:13:15 +0600

    added dynamic routing

---
+++ my-app/src/app/users/[id]/page.js
@@ -0,0 +1,23 @@
+export default async function UserDetails({params}) {
+    const {id} = await params
+
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+    const users = await response.json()
+
+    return(
+        <div className="p-10">
+            <div className="max-w-md mx-auto bg-white shadow-lg rounded-lg overflow-hidden border">
+                <h1 className="text-white text-2xl font-bold">{users.name}</h1>
+            </div>
+
+            <div className="p-6">
+                <p><strong>Username:</strong>{users.username}</p>
+                <p><strong>Email:</strong>{users.email}</p>
+                <p><strong>Phone:</strong>{users.phone}</p>
+                <p><strong>Website:</strong>{users.website}</p>
+                <p className="mt-4"><strong>Company:</strong>{users.company.name}</p>
+            </div>
+            
+        </div>
+    )
+}
